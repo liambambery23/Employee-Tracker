addb37a5c593d71c768e4998e8a0b5fb8c8ec30e
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-01-02 12:50:57 -0500

    corrected issues with schema

--- schema.sql
+++ schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE _role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-    title VARCHAR(30);
-    salary DECIMAL;
-    department_id INT;
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
